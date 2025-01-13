28154412bd67c9e4eb40ee26416ba1b692de8c1e
ihufu <7317411@gmail.com>
2025-01-13 16:27:56 +0800

    Revert "feat: initial project structure"
    This reverts commit b191836ca04f1e947f004db1f65849b4e20fda04.

--- rule_engine/internal/model/message.go
+++
@@ -1,7 +0,0 @@
-package model
-
-// Message 消息结构
-type Message struct {
-	Event string `json:"event"` // 事件类型
-	Data  any    `json:"data"`  // 事件数据
-}
--- rule_engine/internal/model/response.go
+++
@@ -1,8 +0,0 @@
-package model
-
-// Response 通用响应结构
-type Response struct {
-	Code    int         `json:"code"`
-	Message string      `json:"message"`
-	Data    interface{} `json:"data,omitempty"`
-}
